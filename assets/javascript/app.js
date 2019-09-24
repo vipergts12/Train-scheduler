99c67b4e409896cba1ff11e500a152a6434538d3
Oscar Enriquez <macair@unknowna483e7a80242.home>
2019-09-23 21:28:11 -0400

    started front end and signed up for transport api

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+console.log("hey it worked");
+$(document).ready(function() {
+    var whereTo = $("#to");
+    var whereFrom = $("#from");
+    var whereTravel = $("#travel");
+    var departsWhere = $("#departs");
+    var arriveWhere = $("#arrive");
+    
+    whereTo.text("");
+    whereFrom.text("");
+    whereTravel.text("");
+    departsWhere.text("");
+    arriveWhere.text("");
+
+
+
+
+
+});
